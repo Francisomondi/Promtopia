6a52778a9add64671ad9f575892cdd8533976c0f
francis omondi <francisomondi17@gmail.com>
2023-06-15 17:12:37 +0300

    profile component fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,10 +3,7 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 import '@styles/globals.css'
 
-export const metadata = {
-  title: 'Promtopia',
-  description: 'Discover and share AI prompts',
-}
+
 
 export default function RootLayout({ children }) {
   return (
